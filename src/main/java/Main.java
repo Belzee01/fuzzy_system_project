c8317b228c86e49aecc3b3993caf7abb969f660e
Kajetan Lipensky <kajetanlipensky@gmail.com>
2017-09-03 16:06:15 +0200

    fixing tags

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,6 @@
-import dto.Game;
 import evalutor.FuzzyEvaluator;
-import fileproc.AdvancedFileReader;
-import fileproc.AdvancedFileWriter;
 
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
-import java.util.List;
-import java.util.Random;
 
 public class Main {
 
